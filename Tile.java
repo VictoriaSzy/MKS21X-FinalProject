477f290e47dbf0f1970ef5e8e8dd8778e8c56d11
VictoriaSzy <43052165+VictoriaSzy@users.noreply.github.com>
2019-01-03 17:18:38 -0500

    Added constructor and accessor methods

--- Tile.java
+++ Tile.java
@@ -1,3 +1,36 @@
-public class Tile{
-
+public class Tile {
+  // Variables are here:
+  private boolean hasBomb, visible, flagged ;
+  private String identifier ;
+  
+  /** Constructor
+    *@param isABomb is a boolean that will determine whether the tile being created is going to become a bomb, which requires hasBomb to be true
+    */
+  public Tile(boolean isABomb) {
+    if (isABomb) {
+      hasBomb = true ;
+      identifier = "*" ;
+    }
+    else {
+      hasBomb = false ;
+      identifier = "0" ;
+    }
+    visible, flagged = false ;
+  }
+  
+  // Accessors Methods
+  public boolean isBomb() {
+    return hasBomb ;
+  }
+  public boolean isVisble() {
+    return visible ;
+  }
+  public boolean isFlagged() {
+    return flagged ;
+  }
+  public String getIdentifier() {
+    return identifier ;
+  }
+  // Mutator Methods
+  public void setIdentifier(String n)
 }
