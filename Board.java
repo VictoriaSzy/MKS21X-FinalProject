9618486a9b5c7c2296e07c56f55ecd73cd97fef5
VictoriaSzy <43052165+VictoriaSzy@users.noreply.github.com>
2019-01-03 17:00:11 -0500

    Added Variables and frames for methods

--- Board.java
+++ Board.java
@@ -1,3 +1,21 @@
-public class Board{
+import java.util.* ;
+import java.io.* ;
 
+public class Board {
+  // Variables:
+  private Tile[][] layout ;
+  private int[] locationsOfBombs ;
+  private String mode ;
+  private int numberFlagged ;
+  
+  // Constructor
+  public Board(String mode) {
+    
+  }
+  
+  // We can use this to print out a visual representation of the board
+  public String toString() {
+    
+  }
+  
 }
