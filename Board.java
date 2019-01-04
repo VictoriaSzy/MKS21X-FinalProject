0cd97ed761f57c540eb49662c5a81f0ebfd8784c
Jionghao <jwu03@stuy.edu>
2019-01-04 11:49:50 -0500

    added tostring

--- Board.java
+++ Board.java
@@ -7,15 +7,24 @@ public class Board {
   private int[] locationsOfBombs ;
   private String mode ;
   private int numberFlagged ;
-  
+
   // Constructor
   public Board(String mode) {
-    
   }
-  
+
   // We can use this to print out a visual representation of the board
   public String toString() {
-    
+    String result = "";
+    for(int i =0; i < layout.length; i++){
+      for(int n =0; n < layout[0].length; n++){
+        if (layout[i][n].isVisible()){
+        result = result + layout[i][n].getIdentifier() + " ";
+      }else{
+        result = result + "  ";
+      }
+      }
+    }
+    return result;
   }
-  
+
 }
