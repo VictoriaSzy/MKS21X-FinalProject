cbde653e180d201972830fea5d9ebc30fce776f2
jwu03 <jwu03@stuy.edu>
2019-01-03 21:30:39 -0500

    created the rest of the methods in tile class

--- Tile.java
+++ Tile.java
@@ -2,7 +2,7 @@ public class Tile {
   // Variables are here:
   private boolean hasBomb, visible, flagged ;
   private String identifier ;
-  
+
   /** Constructor
     *@param isABomb is a boolean that will determine whether the tile being created is going to become a bomb, which requires hasBomb to be true
     */
@@ -17,7 +17,7 @@ public class Tile {
     }
     visible, flagged = false ;
   }
-  
+
   // Accessors Methods
   public boolean isBomb() {
     return hasBomb ;
@@ -31,6 +31,25 @@ public class Tile {
   public String getIdentifier() {
     return identifier ;
   }
+  public int numberOfBombsAround(){
+    //not implemented yet
+  }
+
   // Mutator Methods
-  public void setIdentifier(String n)
+  public void setIdentifier(String n){
 }
+//action methods
+  public void flag(){
+  }
+
+  public void unflag(){
+  }
+
+  public void reveal(){
+  }
+
+  public void blowUpNormal(){
+  }
+
+  public void blowUpCrazy(){
+  }
