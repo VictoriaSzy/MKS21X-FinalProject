0c70291ad1261e6d78d8a9a0a6cd46cec11dbde4
VictoriaSzy <vszymanowski00@stuy.edu>
2019-01-13 20:24:11 -0500

    Made a comment on possibly rewriting part of while loop in one of the methods

--- EasyBoard.java
+++ EasyBoard.java
@@ -1,75 +1,70 @@
 public class EasyBoard {
-public String[][] layout = new String[12][12];   //12 rows, 12 columns.
- public String[][] display = new String[12][12]; //This is the field that is visible to the player.
- public boolean complete = false ;
+public String[][] layout = new String[12][12] ;   // 12 rows, 12 columns
+ public String[][] display = new String[12][12] ; // This is what the user sees
+ public boolean finished = false ;
  public boolean victory = false ;
 
- private String hidden = " ? ";
- private String bomb = " * ";
- private String empty = "   ";
+ private String hidden = " ? " ;
+ private String bomb = " * " ;
+ private String empty = "   " ;
 
  public EasyBoard(){
-    int row = 0;
-    int column = 0;
-    for(int x = 0; x < layout.length; x++){
-      for(int y = 0; y < layout[0].length ; y++){
-        //Places blank spaces in buffer zones.
-        if((x == 0 || x == layout.length - 1)||(y == 0 || y == layout[0].length - 1)){
-          layout[x][y] = empty;
-          display[x][y] = empty;
+    int row = 0 ;
+    int column = 0 ;
+    for(int x = 0 ; x < layout.length ; x++){
+      for(int y = 0 ; y < layout.length ; y++){
+        if((x == 0 || x == layout.length - 1)||(y == 0 || y == layout.length - 1)){
+          layout[x][y] = empty ;
+          display[x][y] = empty ;
         }
-        //Places ? in game field.
+        // Otherwise put a "?" because the player doesn't know what it is
         else{
-          layout[x][y] = hidden;
-          display[x][y] = hidden;
+          layout[x][y] = hidden ;
+          display[x][y] = hidden ;
         }
       }
     }
   }
 
-  //Displays the layout.
+  //Displays the layout, sort of like the toString
   public static void printGame(String[][] str){
-    for(int x = 1; x < str.length - 1; x++){
-      for(int y = 0; y < str[0].length ; y++){
-        //Formats row.
-        if(y > 0 && y < str[0].length)
-          System.out.print("|");
-        else
-          System.out.println("");
-
-        System.out.print(str[x][y]);  //Prints out content of each tile.
+    for(int x = 1 ; x < str.length - 1 ; x++){
+      for(int y = 0 ; y < str[0].length ; y++){
+        if (y > 0 && y < str[0].length) System.out.print("|") ;
+        else {
+          System.out.println("") ;
+        }
+        System.out.print(str[x][y]) ;  // Prints out the tile
       }
     }
   }
-
-  //Places n mines at random on the field.
-public void generateMines(int n){
-  for(int m = 0; m < n; m++){
-    //Loops until a mine is placed.
-    while(true){
-      int x, y = 0;   //Clears vars.
-      x = (int)(10*Math.random());
-      y = (int)(10*Math.random());
-
-      //So that a mine is placed in a tile visible to the player.
-      if(x >= 1 && x <= 10){
-        if(y >= 1 && y <= 10){
-          //Checks if a mine is present in a spot.
-          if(!field[x][y].equals(bomb)){
-            field[x][y] = bomb;
-            break;
+  // Randomly places the bombs
+  public void generateMines(int n){
+    for(int m = 0; m < n; m++){
+      boolean generatingAMine = true ;
+      while(generatingAMine){
+        int x, y = 0 ;
+        x = (int)(10*Math.random()) ;
+        y = (int)(10*Math.random()) ;
+        //So that a mine is placed in a tile visible to the player.
+        if(x >= 1 && x <= 10){
+          if(y >= 1 && y <= 10){
+            //Checks if a mine is already there
+            if(!layout[x][y].equals(bomb)){
+              layout[x][y] = bomb;
+              break ; // maybe we can find a way to stop this --> change the boolean you were using
+            }
           }
         }
       }
     }
   }
-}
 
 //On first move, this clears the area around the selected tile.
 public void clear(int x, int y){
   for(int i = (x - 1); i <= (x + 1); i++){
     for(int j = (y - 1); j <= (y + 1); j++){
-      if(field[i][j].equals(hidden) == true){
+      if(layout[i][j].equals(hidden) == true){
         display[i][j] = empty;
         layout[i][j] = empty;
       }
