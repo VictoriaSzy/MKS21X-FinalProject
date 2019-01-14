a5831a3695092142b264eebce10ee70423c19993
VictoriaSzy <vszymanowski00@stuy.edu>
2019-01-14 11:43:45 -0500

    Gave parameters to go method based on the number of bombs to be generated and the size

--- Minesweeper.java
+++ Minesweeper.java
@@ -2,23 +2,33 @@ public class Minesweeper{
   //this just runs the simple Minesweeper
   public static void main(String[] args) {
     String mo, lev ;
+    int randomNumberOfBombs, len ;
+    Random ran = new Random() ;
     if (args[0].equals("NE")) {
       mo = "Normal" ;
       lev = "Easy" ;
+      randomNumberOfBombs = Math.abs(ran.nextInt() % 3) + 3 ;
+      len = 5 ;
     }
     if (args[0].equals("NH")) {
       mo = "Normal" ;
       lev = "Hard" ;
+      randomNumberOfBombs = Math.abs(ran.nextInt() % 7) + 6 ;
+      len = 8 ;
     }
     if (args[0].equals("CE")) {
       mo = "Crazy" ;
       lev = "Easy" ;
+      randomNumberOfBombs = Math.abs(ran.nextInt() % 11) + 10 ;
+      len = 8 ;
     }
     if (args[0].equals("CH")) {
       mo = "Crazy" ;
       lev = "Hard" ;
+      randomNumberOfBombs = Math.abs(ran.nextInt() % 6) + 5 ;
+      len = 10 ;
     }
     BoardMethods.start() ;
-    BoardMethods.go(12) ;
+    BoardMethods.go(randomNumberOfBombs, len) ;
   }
 }
