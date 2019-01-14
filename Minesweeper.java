0cc045c4c6b13d1d83f45648351071edc8b55af2
VictoriaSzy <vszymanowski00@stuy.edu>
2019-01-14 11:37:40 -0500

    Added reading parameters to determine mode and level

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,6 +1,23 @@
 public class Minesweeper{
   //this just runs the simple Minesweeper
   public static void main(String[] args) {
+    String mo, lev ;
+    if (args[0].equals("NE")) {
+      mo = "Normal" ;
+      lev = "Easy" ;
+    }
+    if (args[0].equals("NH")) {
+      mo = "Normal" ;
+      lev = "Hard" ;
+    }
+    if (args[0].equals("CE")) {
+      mo = "Crazy" ;
+      lev = "Easy" ;
+    }
+    if (args[0].equals("CH")) {
+      mo = "Crazy" ;
+      lev = "Hard" ;
+    }
     BoardMethods.start() ;
     BoardMethods.go(12) ;
   }
