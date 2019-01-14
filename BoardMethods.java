bf025bf0ab999fa53a59ea16486f93f93ca2502a
VictoriaSzy <vszymanowski00@stuy.edu>
2019-01-13 20:33:53 -0500

    Created new file that will have all of the methods together to minimize the number of classes

---
+++ BoardMethods.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class BoardMethods{
+
+  public static void start() {
+    // this just prints out the directions so the user understands how to play
+    System.out.println("Why are you here? To play the most awesomest game ever: Minesweeer!") ;
+    System.out.println("In Normal Mode, you have to try to find where the bombs or mines are, and AVOID THEM. Be careful to not accidentally choose one of them!") ;
+    System.out.println("In Crazy Mode, you have to try to find where the bombs or mines are, and AVOID THE REGULAR TILES!") ;
+    System.out.println("Use the tiles around to determine whether there is a bomb or mine near.") ;
+    System.out.println("To choose a tile, enter the coordinates of the tile you want to select.") ;
+    System.out.println("The size of the Normal Easy Mode Board is 5x5, while the hard mode has a size of 8x8.") ;
+    System.out.println("The size of the Crazy Easy Mode Board is 8x8, while the hard mode has a size of 10x10.") ;
+    System.out.println("Good luck!") ;
+  }
+}
